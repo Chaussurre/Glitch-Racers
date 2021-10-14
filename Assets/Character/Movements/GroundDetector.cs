e07c89dbef4150f2c371b702c7ba3126131fb290
Chaussurre <Samuel@Corno.com>
2021-10-14 20:14:01 +0200

    Sloppy ground detection

--- Assets/Character/Movements/GroundDetector.cs
+++ Assets/Character/Movements/GroundDetector.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Character
 {
@@ -8,16 +11,20 @@ namespace Character
      */
     public class GroundDetector : MonoBehaviour
     {
+        [SerializeField, Range(0, 90)]
+        [Tooltip("The angle at wich the ground is still walkable")]
+        float AngleTolerance = 10;
 
-        readonly HashSet<Collision> collisions = new HashSet<Collision>();
 
-        Vector3 normal;
+        readonly private HashSet<Collision> collisions = new HashSet<Collision>();
+
+        private Vector3 normal;
         //! true if the character is on walkable ground
         public bool OnGround
         {
             get
             {
-                return normal == Vector3.up;
+                return Vector3.Angle(Vector3.up, normal) <= AngleTolerance;
             }
         }
 
@@ -49,5 +56,19 @@ namespace Character
         {
             collisions.Remove(collision);
         }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            int rayCount = 20;
+            for (int i = 0; i < rayCount; i++)
+            {
+                Gizmos.color = Color.magenta;
+                Gizmos.DrawRay(transform.position + Vector3.down, Quaternion.Euler(0, 360 / rayCount * i, AngleTolerance) * Vector3.up);
+                Gizmos.color = Color.green;
+                Gizmos.DrawRay(transform.position + Vector3.down, Quaternion.Euler(0, 360 / rayCount * i, AngleTolerance + 90) * Vector3.up);
+            }
+        }
+#endif
     }
 }
