ca63730b6ea48194664a70fdd8487a4a2bbaf98e
Théo <dubuissontheo@outlook.fr>
2021-11-08 21:27:08 +0100

    Path PathTarget

--- Source/Assets/Character/AI/PathTarget.cs
+++ Source/Assets/Character/AI/PathTarget.cs
@@ -10,11 +10,39 @@ namespace Character
 {
     public class PathTarget : MonoBehaviour
     {
+        [SerializeField]
+        private bool jump;
+        //private Vector3 startJump;
+        //private Vector3 endJump;
+
+        [SerializeField]
+        private Vector3 jumpDirection;
+        
+        
+        
 #if UNITY_EDITOR
         void OnDrawGizmos()
         {
             Gizmos.color = Color.green;
             Gizmos.DrawSphere(transform.position, 1);
+        }
+        
+        void OnDrawGizmosSelected()
+        {
+            if (jump)
+            {
+                Handles.Label(transform.position + jumpDirection, "jump");
+                Gizmos.color = Color.magenta;
+                for (float d = 0.1f; d < jumpDirection.magnitude; d += 0.1f)
+                {
+                    Gizmos.DrawLine(transform.position + GetCurvePoint(d-0.1f), transform.position + GetCurvePoint(d));
+                }
+            }
+        }
+
+        Vector3 GetCurvePoint(float distance)
+        {
+            return jumpDirection.normalized * distance;
         }
 #endif
     }
