1dd431b0ebb7003f9866f12e80e34591af98afbf
Pierre Durand <pierre.durand95360@gmail.com>
2022-03-29 08:46:52 +0200

    Pause script implemented

--- Source/Assets/PauseScript.cs
+++ Source/Assets/PauseScript.cs
@@ -3,23 +3,41 @@ using UnityEngine;
 
 public class PauseScript : MonoBehaviour
 {
-    [SerializeField] private GameObject pauseMenu;
+    private GameObject[] objects;
     public GameObject test;
 
     private void Pause()
     {
+        Cursor.lockState = CursorLockMode.None;
         if (!Input.GetKeyDown(KeyCode.Escape)) return;
-        pauseMenu.SetActive(true);
+        foreach (var obj in objects)
+        {
+            obj.SetActive(true);
+        }
         Time.timeScale = 0f;
         test.SetActive(false);
     }
     
     public void Resume()
     {
-        pauseMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Confined;
+        GameObject.FindWithTag("PauseObject").SetActive(false);
         Debug.Log("Resuming");
         Time.timeScale = 1f;
+        foreach (var obj in objects)
+        {
+            obj.SetActive(false);
+        }
         test.SetActive(true);
+    }
+
+    private void Start()
+    {
+        objects = GameObject.FindGameObjectsWithTag("PauseObject");
+        foreach (var obj in objects)
+        {
+            obj.SetActive(false);
+        }
     }
 
     private void Update()
